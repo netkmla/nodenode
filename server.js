61adb79463d0efd8200f94457cc35aeed84333c4
Roh Geonho <roh517@naver.com>
2019-09-07 04:01:42 +0900

    get

--- server.js
+++ server.js
@@ -8,10 +8,25 @@ app.get('/',function(req, res){
   res.sendFile(__dirname + '/client.html');
 });
 
+var url = require('url');
+var app = http.createServer(function(request,response){ 
+  var _url = request.url; 
+  var queryData = url.parse(_url,true).query;
+  var title = queryData.id;
+});
+
+
 var count=1;
 io.on('connection', function(socket){
   console.log('user connected: ', socket.id);
-  var name = "user" + count++;
+  if(title == null)
+  {
+    var name = "user" + count++;
+  }
+  else
+  {
+    var name = title;
+  }
   io.to(socket.id).emit('change name',name);
 
   socket.on('disconnect', function(){
